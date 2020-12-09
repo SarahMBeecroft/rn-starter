427ee4894362f801ecd6139d263107f7b431e5bc
Sarah Beecroft <44879145+SarahMBeecroft@users.noreply.github.com>
2020-12-08 18:54:37 -0800

    More practice

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ColorCounter from '../components/ColorCounter';
+
+const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
-  return <Text>Square Screen</Text>;
+  // initialize variables with useState
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+  return (
+    <View>
+      <ColorCounter
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
+        color='Red'
+      ></ColorCounter>
+      <ColorCounter
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        color='Blue'
+      ></ColorCounter>
+      <ColorCounter
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        color='Green'
+      ></ColorCounter>
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
