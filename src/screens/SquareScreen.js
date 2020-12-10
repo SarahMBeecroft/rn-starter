529c48c8c24f7ad6ba1eaa6a5b40f886e02b7d1d
Sarah Beecroft <44879145+SarahMBeecroft@users.noreply.github.com>
2020-12-09 19:27:01 -0800

    More react-native study

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,25 +5,54 @@ import ColorCounter from '../components/ColorCounter';
 const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
-  // initialize variables with useState
+  // Initialize variables with useState
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+
+  // Helper function to ensure color doesn't exceed 255 or go negative
+  const setColor = (color, change) => {
+    console.log(color);
+    // color === 'red', 'green', 'blue'
+    // change === +15, -15
+    if (color === 'red') {
+      if (red + change > 255 || red + change < 0) {
+        return;
+      } else {
+        setRed(red + change);
+      }
+    }
+    if (color === 'blue') {
+      if (blue + change > 255 || blue + change < 0) {
+        return;
+      } else {
+        setBlue(blue + change);
+      }
+    }
+    if (color === 'green') {
+      if (green + change > 255 || green + change < 0) {
+        return;
+      } else {
+        setGreen(green + change);
+      }
+    }
+  };
+
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + COLOR_INCREMENT)}
-        onDecrease={() => setRed(red - COLOR_INCREMENT)}
+        onIncrease={() => setColor('red', COLOR_INCREMENT)}
+        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
         color='Red'
       ></ColorCounter>
       <ColorCounter
-        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+        onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
         color='Blue'
       ></ColorCounter>
       <ColorCounter
-        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        onIncrease={() => setColor('green', COLOR_INCREMENT)}
+        onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
         color='Green'
       ></ColorCounter>
       <View
@@ -31,6 +60,9 @@ const SquareScreen = () => {
           height: 150,
           width: 150,
           backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+          margin: 30,
+          alignSelf: 'center',
+          borderRadius: 15,
         }}
       />
     </View>
