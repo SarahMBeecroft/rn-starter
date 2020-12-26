446c407dd15dbd54138bc2192a5c48c3db9220f0
Sarah Beecroft <44879145+SarahMBeecroft@users.noreply.github.com>
2020-12-26 14:33:22 -0800

    Refactored counter component to use reducer instead of useState

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+// Reducer Function
+const reducer = (state, action) => {
+  // state === { count: number }
+  // action === { type: 'increase_count' || 'decrease_count, payload: 1 }
+  switch (action.type) {
+    case 'increase_count':
+      return { ...state, count: state.count + action.payload };
+    case 'decrease_count':
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const { count } = state;
+
   return (
     <View>
       <Button
         title='Increase'
         onPress={() => {
-          setCounter(counter + 1);
-          console.log(counter);
+          dispatch({ type: 'increase_count', payload: 1 });
         }}
       ></Button>
       <Button
         title='Decrease'
         onPress={() => {
-          setCounter(counter - 1);
-          console.log(counter);
+          dispatch({ type: 'decrease_count', payload: 1 });
         }}
       ></Button>
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {state.count}</Text>
     </View>
   );
 };
